6cbc276f95b8c61b6eb117d65e434ecf772cc852
Jong9106 <juank_hr@hotmail.com>
2022-11-28 19:16:45 -0500

    ADD | New file

--- 0x00-hello-dart/4-print_string.dart
+++ 0x00-hello-dart/4-print_string.dart
@@ -1,5 +1,5 @@
-void main() {
+void main() { // Print string 3 times, then first 9 characters
   String str = "Holberton School";
-    print(str*3);
-    print("Holberton\n");
+  print(str * 3); // * is String multiply operator
+  print(str.substring(0, 9) + '\n'); //substring is like slice - start inclusive to end exclusive (optional)
 }
